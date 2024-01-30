b70b9df55734da1e45e47f17b542883167d150fe
Mustafa Dalkılıç <81879117+MustafaD3@users.noreply.github.com>
2024-01-31 02:07:03 +0300

    Add files via upload

---
+++ shell01/ex07/r_dwssap.sh
@@ -0,0 +1,14 @@
+export FT_LINE1=7
+export FT_LINE2=15
+cat /etc/passwd \
+| sed '/^#/d' \
+| awk 'NR % 2 == 0 {print $0}' \
+| awk -F ":" '{print $1}' \
+| rev \
+| sort -fnr \
+| awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR <= FT_LINE2 {print $0}' \
+| tr '\n' ' ' \
+| sed 's+ +, +g' \
+| sed 's+, $+.+g' \
+| tr -d '\n'
+
